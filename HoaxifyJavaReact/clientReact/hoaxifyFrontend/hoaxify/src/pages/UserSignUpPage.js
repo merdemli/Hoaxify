876b5d0031a3e6447ea96e2d8a9f8b372ad6ed8c
merdemli <57098477+merdemli@users.noreply.github.com>
2021-08-26 01:50:33 +0300

    Validation for UserSignUpPage

--- HoaxifyJavaReact/clientReact/hoaxifyFrontend/hoaxify/src/pages/UserSignUpPage.js
+++ HoaxifyJavaReact/clientReact/hoaxifyFrontend/hoaxify/src/pages/UserSignUpPage.js
@@ -1,36 +1,41 @@
 import React from 'react';
-import {signup} from '../api/apiCall';
+import { signup } from '../api/apiCall';
 
-class UserSignUpPage extends React.Component{
+class UserSignUpPage extends React.Component {
 
     state = {
         username: null,
         displayName: null,
         password: null,
         passwordConfirm: null,
-        pendingApiCall:false
+        pendingApiCall: false,
+        errors: {}
 
     };
 
-        //onChange = event => {
-       //const value = event.target.value;
-        // const name = event.target.name;
-        // this.setState({
-        //     [name] : value   //name ne ise ona karsı girilen degeri state'de 
-        //                      //güncelliyor olacak,generic bir yapı kuruldu
-        //                      //aynı onChange fonksiyonu tüm field'lar için çalışıyor hale  geldi
-        // })
-//}
+    //onChange = event => {
+    //const value = event.target.value;
+    // const name = event.target.name;
+    // this.setState({
+    //     [name] : value   //name ne ise ona karsı girilen degeri state'de 
+    //                      //güncelliyor olacak,generic bir yapı kuruldu
+    //                      //aynı onChange fonksiyonu tüm field'lar için çalışıyor hale  geldi
+    // })
+    //}
 
     onChange = event => {
-                                             //generic yapı
-        const { name, value } =event.target //object costructring;   event'in target objesi 
-                                             //içindeki name'i al,target'daki value degerini tanımla
-        this.setState({
-         [name] : value                      
+        //generic yapı
+        const { name, value } = event.target //object costructring;   event'in target objesi 
+        //içindeki name'i al,target'daki value degerini tanımla
+        const errors = {...this.state.errors};
+        errors[name] = undefined; //var olan error caseini ortadan kaldırıyoruz
 
-    });
-};
+        this.setState({
+            [name]: value,
+            errors
+
+        });
+    };
 
     // onChangeUsername = event => {
     //     this.setState({
@@ -56,71 +61,85 @@ class UserSignUpPage extends React.Component{
 
     // };
 
-    onClickSignUp = async event => {
+    onClickSignUp = async event => { //async: fonx sonucunun promise oldugunu belirtir
 
         event.preventDefault();
 
-        const {username, displayName, password} = this.state;
+        const { username, displayName, password } = this.state;
 
         const body = {       //json objesi olacak
             username, //username,                 
-            displayName, // displayName,           //key ve value için kullanılan isimlendirmeler aynıysa, sadece ir tanesini yazmamız yeterli olacaktır.
+            displayName, // displayName,         //key ve value için kullanılan isimlendirmeler aynıysa, sadece ir tanesini yazmamız yeterli olacaktır.
             password, //password
         };
 
-        this.setState({pendingApiCall: true});
+        this.setState({ pendingApiCall: true });
 
         try {
 
-            const response = await signup(body);
-            
+            const response = await signup(body); //sonucu promise olan fonksiyonun  bitmesini bekler
+            //this.setState({pendingApiCall: false})
+
         } catch (error) {
-           this.setState({pendingApiCall: false}) ;
+
+            if(error.response.data.validationErrors){
+                this.setState({ errors: error.response.data.validationErrors });
+            }
+            //this.setState({pendingApiCall: false}) ;
+            
         }
+        this.setState({ pendingApiCall: false });
         //axios.post('/api/1.0/users',body)
-        //signup(body)
+
+        //signup(body) //promise bir çağrı
         //.then((response)=>{
-         //   this.setState({pendingApiCall: false});
+        //   this.setState({pendingApiCall: false});
         //}).catch(error =>{
         //    this.setState({pendingApiCall: false});
-       // })
+        // })
     }; //callback function
 
-    
+
 
     render() {
 
-        const { pendingApiCall } = this.state;
+        const { pendingApiCall, errors } = this.state;
+        const{ username, displayName} = errors;
         return (
-            <div className="container"> 
-            <form>
-                <h1 className="text-center" >Sign Up</h1>  
-                <div className="mb-3">
-                    {<label>Username</label>           /*this.onChangeUsername--> */}
-                    <input className="form-control" name = "username" onChange={this.onChange} />
-                </div>
+            <div className="container">
+                <form>
+                    <h1 className="text-center" >Sign Up</h1>
+                    <div className="mb-3">
+                        {<label>Username</label>           /*this.onChangeUsername--> */}
+                        <input className={username ? 'form-control is-invalid' : 'form-control'} name="username" onChange={this.onChange} />
+                        <div className="invalid-feedback">
+                           {username}</div>
+                    </div>
 
-                <div className="mb-3">
-                    <label>Display Name </label>
-                    <input className="form-control" name = "displayName" onChange={this.onChange} />
-                </div>
-                <div className="mb-3">
-                    <label>Pasword</label>
-                     <input className="form-control" name ="password" onChange={this.onChange} type="password"/>
-                </div>
-                <div className="mb-3">
-                    <label>Pasword Confirm</label>
-                     <input className="form-control" name = "passwordConfirm" onChange= {this.onChange} type="password"/>
-                </div>
-                <div className= "text-center" >
-                <button className="btn btn-primary"
-                 onClick={this.onClickSignUp}
-                 disabled={pendingApiCall} >
-                {/*expression JS*/pendingApiCall && <span class="spinner-border spinner-border-sm"></span> /*conditional rendering*/} 
-                 Sign Up
-                </button>
-                </div>
-            </form>    
+                    <div className="mb-3">
+                        <label>Display Name </label>
+                        <input className={displayName ? 'form-control is-invalid' : 'form-control'} name="displayName" onChange={this.onChange} />
+                        <div className="invalid-feedback">
+                           {displayName}</div>
+                    </div>
+
+                    <div className="mb-3">
+                        <label>Pasword</label>
+                        <input className="form-control" name="password" onChange={this.onChange} type="password" />
+                    </div>
+                    <div className="mb-3">
+                        <label>Pasword Confirm</label>
+                        <input className="form-control" name="passwordConfirm" onChange={this.onChange} type="password" />
+                    </div>
+                    <div className="text-center" >
+                        <button className="btn btn-primary"
+                            onClick={this.onClickSignUp}
+                            disabled={pendingApiCall} >
+                            {/*expression JS*/pendingApiCall && <span class="spinner-border spinner-border-sm"></span> /*conditional rendering*/}
+                            Sign Up
+                        </button>
+                    </div>
+                </form>
             </div>
 
         );
--- HoaxifyJavaReact/serverJava/ws/src/main/java/com/hoaxify/ws/user/api/controllers/UsersController.java
+++ HoaxifyJavaReact/serverJava/ws/src/main/java/com/hoaxify/ws/user/api/controllers/UsersController.java
@@ -1,12 +1,17 @@
 package com.hoaxify.ws.user.api.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hoaxify.ws.error.ApiError;
 import com.hoaxify.ws.shared.GenericResponse;
 import com.hoaxify.ws.user.abstracts.UserService;
 import com.hoaxify.ws.user.concrete.User;
@@ -25,15 +30,36 @@ public class UsersController {
 
 	@PostMapping("/api/1.0/users")
 	@ResponseStatus(HttpStatus.CREATED)
-	public GenericResponse add(@RequestBody User user) {
+	public ResponseEntity<?> add(@RequestBody User user) {
 		
+		ApiError error = new ApiError(400,"Validation error","/api/1.0/users");
+		Map<String, String>validationErrors= new HashMap<>();
+		
+		String username = user.getUsername();
+		String displayName = user.getDisplayName();
+		
+		if(username==null || username.isEmpty() )
+		{
+			validationErrors.put("username", "username can not be null");
+			
+		}
+		
+		if(displayName==null || displayName.isEmpty() ) {
+			validationErrors.put("displayName", "can not be null");
+		}
+		
+		if(validationErrors.size()>0) {
+			error.setValidationErrors(validationErrors);
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);	
+		}	
 		userService.add(user);
-		return new GenericResponse("User created");
-		
+		return ResponseEntity.ok(new GenericResponse("User created"));
 		
 		
 		
 		
 	}
+	
+	
 
 }
